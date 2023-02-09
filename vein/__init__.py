c42a60a7da1ea7a9c9baba21ee491ae08af4d20d
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2023-02-09 21:10:18 +0100

    Template rendering of all questions

--- vein/__init__.py
+++ vein/__init__.py
@@ -23,9 +23,20 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    questions = [
+        "My enthusiasm regarding the work I do...",
+        "The Teamwork atmosphere and communication during the last sprints were...",
+        "To what extent the tasks were challenging enough for me...",
+        "I would rate my value contributed to the team as follows...",
+        "The workload of this/the last sprint was...",
+        "I feel supported by the client and stakeholders...",
+        "I feel recognized and praised by the team...",
+        "I feel inspired and excited to work in this team for the coming sprints..."
+    ]
+
     @app.route("/")
     def home():
-        return render_template('index.html')
+        return render_template('index.html', questions=questions)
 
     # a simple page that says hello
     @app.route('/hello')
